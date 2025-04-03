654e5945a4f852ffd083c689dd2b0d3c017c7f99
Aayush06122005 <aayush23014@iiitd.ac.in>
2025-04-03 19:21:14 +0530

    api of appwrite

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,122 +1,125 @@
 import conf from '../conf/conf.js';
 import { Client, ID, Databases, Storage, Query } from "appwrite";
-export class Service{
+
+export class Service {
     client = new Client();
     databases;
     bucket;
-    constructor(){
+
+    constructor() {
         this.client
-        .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId);
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+
         this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);
     }
-    async createPost({title, slug, content, featuredImage, status, userId}){
+
+    // 1. Create an Appointment
+    async createAppointment({ userId, hospitalId, dateTime, description }) {
         try {
             return await this.databases.createDocument(
                 conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                slug,
+                conf.appwriteAppointmentCollectionId, // Use a dedicated collection for appointments
+                ID.unique(),
                 {
-                    title,
-                    content,
-                    featuredImage,
-                    status,
                     userId,
+                    hospitalId,
+                    dateTime,
+                    description,
                 }
-            )
+            );
         } catch (error) {
-            console.log("Appwrite serive :: createPost :: error", error);
+            console.log("Appwrite Service :: createAppointment :: error", error);
+            return false;
         }
     }
 
-    async updatePost(slug, {title, content, featuredImage, status}){
-        try {
-            return await this.databases.updateDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                slug,
-                {
-                    title,
-                    content,
-                    featuredImage,
-                    status,
-                }
-            )
-        } catch (error) {
-            console.log("Appwrite serive :: updatePost :: error", error);
-        }
-    }
-    async deletePost(slug){
-        try {
-            await this.databases.deleteDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                slug
-            )
-            return true
-        } catch (error) {
-            console.log("Appwrite serive :: deletePost :: error", error);
-            return false
-        }
-    }
-    async getPost(slug){
-        try {
-            return await this.databases.getDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                slug
-            )
-        } catch (error) {
-            console.log("Appwrite serive :: getPost :: error", error);
-            return false
-        }
-    }
-    async getPosts(queries = [Query.equal("status", "active")]){
+    // 2. Get Appointments for a User
+    async getAppointments(userId) {
         try {
             return await this.databases.listDocuments(
                 conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                queries,
-            )
+                conf.appwriteAppointmentCollectionId,
+                [Query.equal("userId", userId)]
+            );
         } catch (error) {
-            console.log("Appwrite serive :: getPosts :: error", error);
-            return false
-        }
-    }
-    // file upload service
-    async uploadFile(file){
-        try {
-            return await this.bucket.createFile(
-                conf.appwriteBucketId,
-                ID.unique(),
-                file
-            )
-        } catch (error) {
-            console.log("Appwrite serive :: uploadFile :: error", error);
-            return false
+            console.log("Appwrite Service :: getAppointments :: error", error);
+            return false;
         }
     }
 
-    async deleteFile(fileId){
+    // 3. Upload Medical Report (Image)
+    async uploadMedicalReport(file) {
         try {
-            await this.bucket.deleteFile(
-                conf.appwriteBucketId,
-                fileId
-            )
-            return true
+            return await this.bucket.createFile(
+                conf.appwriteMedicalReportsBucketId, // Separate bucket for medical reports
+                ID.unique(),
+                file
+            );
         } catch (error) {
-            console.log("Appwrite serive :: deleteFile :: error", error);
-            return false
+            console.log("Appwrite Service :: uploadMedicalReport :: error", error);
+            return false;
         }
     }
-    getFilePreview(fileId){
+
+    // 4. Get Medical Report Preview URL
+    getMedicalReportPreview(fileId) {
         return this.bucket.getFilePreview(
-            conf.appwriteBucketId,
+            conf.appwriteMedicalReportsBucketId,
             fileId
-        )
+        );
+    }
+
+    // 5. Delete Medical Report
+    async deleteMedicalReport(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteMedicalReportsBucketId,
+                fileId
+            );
+            return true;
+        } catch (error) {
+            console.log("Appwrite Service :: deleteMedicalReport :: error", error);
+            return false;
+        }
+    }
+
+    // 6. Update Patient Personal Info
+    async updatePatientInfo(userId, { name, age, gender, contact, address }) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwritePatientsCollectionId, // Use a collection for patient details
+                userId, // Use the user's ID as the document ID
+                {
+                    name,
+                    age,
+                    gender,
+                    contact,
+                    address,
+                }
+            );
+        } catch (error) {
+            console.log("Appwrite Service :: updatePatientInfo :: error", error);
+            return false;
+        }
+    }
+
+    // 7. Get Patient Personal Info
+    async getPatientInfo(userId) {
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwritePatientsCollectionId,
+                userId
+            );
+        } catch (error) {
+            console.log("Appwrite Service :: getPatientInfo :: error", error);
+            return false;
+        }
     }
 }
 
-const service = new Service()
-export default service 
+const service = new Service();
+export default service;
